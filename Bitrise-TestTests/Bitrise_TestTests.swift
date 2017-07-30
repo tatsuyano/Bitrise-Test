55d8f4718e5158955532f7625e50dd44fdd82041
tatsuyano <tatsuyano@gmail.com>
2017-07-31 00:21:27 +0900

    Initial commit

---
+++ Bitrise-TestTests/Bitrise_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bitrise_TestTests.swift
+//  Bitrise-TestTests
+//
+//  Created by Tatsuya Noguchi on 2017/07/31.
+//  Copyright © 2017年 Tatsuya Noguchi. All rights reserved.
+//
+
+import XCTest
+@testable import Bitrise_Test
+
+class Bitrise_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
